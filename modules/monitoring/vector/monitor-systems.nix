eda7370945a92bc9d26e1a4fbd72edd2a7fde75b
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-11 04:09:37 +0900

    feat(logging): Add vector

---
+++ modules/monitoring/vector/monitor-systems.nix
@@ -0,0 +1,82 @@
+# Vector collector configuration for rho
+# - Local Loki sink for logs
+# - Prometheus server with remote write receiver
+# - Vector exporter for local metrics
+{config, ...}: let
+  wgMgntAddr = config.networking.sbee.currentHost.wg-mgnt;
+in {
+  imports = [
+    ./default.nix
+    ../loki.nix
+  ];
+
+  services.vector.settings.sinks = {
+    # SSH logs to local Loki
+    ssh_logs_local = {
+      type = "loki";
+      inputs = ["filter_ssh"];
+      endpoint = "http://127.0.0.1:3100";
+      encoding.codec = "json";
+      labels = {
+        host = "{{ host }}";
+        log_type = "{{ log_type }}";
+        event = "{{ event }}";
+        user = "{{ user }}";
+      };
+    };
+
+    # Audit logs to local Loki
+    audit_logs_local = {
+      type = "loki";
+      inputs = ["filter_audit"];
+      endpoint = "http://127.0.0.1:3100";
+      encoding.codec = "json";
+      labels = {
+        host = "{{ host }}";
+        log_type = "{{ log_type }}";
+        event = "{{ event }}";
+        user = "{{ user }}";
+      };
+    };
+
+    system_metrics_local = {
+      type = "prometheus_exporter";
+      inputs = ["tag_metrics"];
+      address = "${wgMgntAddr}:9598";
+    };
+  };
+
+  # Prometheus server
+  services.prometheus = {
+    enable = true;
+    listenAddress = wgMgntAddr;
+
+    # enable remote write receiver
+    extraFlags = [
+      "--web.enable-remote-write-receiver"
+      "--storage.tsdb.retention.time=30d"
+    ];
+
+    # scrab local vector metric
+    scrapeConfigs = [
+      {
+        job_name = "vector";
+        scrape_interval = "60s";
+        static_configs = [
+          {
+            targets = ["${wgMgntAddr}:9598"];
+          }
+        ];
+      }
+    ];
+  };
+
+  networking.firewall.interfaces."wg-mgnt".allowedTCPPorts = [
+    9090 # Prometheus
+    9598 # Vector exporter
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/prometheus2 0700 prometheus prometheus - -"
+  ];
+}
