87ce186e247ff1d8063941d85c8980a48ba5d077
mulatta <67085791+mulatta@users.noreply.github.com>
2025-10-11 04:09:37 +0900

    feat(logging): Add prometheus

---
+++ modules/monitoring/prometheus/default.nix
@@ -0,0 +1,19 @@
+# Prometheus alertmanager and rules configuration
+# Note: Prometheus server itself is configured in vector/monitor-systems.nix
+{config, ...}: let
+  wgMgntAddr = config.networking.sbee.currentHost.wg-mgnt;
+in {
+  imports = [
+    ./rules.nix
+  ];
+
+  # TODO: enable alertmanager when ntfy integration is ready
+  services.prometheus.alertmanager = {
+    enable = false;
+    listenAddress = wgMgntAddr;
+    port = 9093;
+  };
+
+  # Open firewall for alertmanager (when enabled)
+  # networking.firewall.interfaces."wg-mgnt".allowedTCPPorts = [9093];
+}
