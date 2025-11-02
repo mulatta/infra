f03c7af0c047e6fa06afd0dfe4f424bb90b5b527
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-02 15:57:35 +0900

    feat(pkgs): add nvidia packages on nix-ld

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,12 +1,25 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  hasNvidia = builtins.elem "nvidia" config.services.xserver.videoDrivers;
+in {
   programs.nix-ld = {
     enable = true;
-    libraries = with pkgs; [
-      stdenv.cc.cc.lib
-      openssl
-      zlib
-      curl
-      libGL
-    ];
+    libraries = with pkgs;
+      [
+        stdenv.cc.cc.lib
+        openssl
+        zlib
+        curl
+        libGL
+      ]
+      ++ lib.optionals hasNvidia [
+        config.boot.kernelPackages.nvidiaPackages.production
+        cudaPackages.cuda_cudart
+        cudaPackages.cudnn
+      ];
   };
 }
