2ab541edf4401a5ddbbf8b518a3733e41ac51014
mulatta <67085791+mulatta@users.noreply.github.com>
2025-11-07 14:40:05 +0900

    feat(terraform): Add media bucket

--- terraform/minio/users.tf
+++ terraform/minio/users.tf
@@ -1,30 +1,4 @@
-resource "minio_s3_bucket" "uniprot" {
-  bucket        = "uniprot"
-  force_destroy = false
-}
-
-resource "minio_s3_bucket" "ncbi" {
-  bucket        = "ncbi"
-  force_destroy = false
-}
-
-resource "minio_s3_bucket" "tfstate" {
-  bucket        = "tfstate"
-  force_destroy = false
-}
-
-resource "minio_s3_bucket" "project-irr" {
-  bucket        = "project-irr"
-  force_destroy = false
-}
-
-resource "minio_s3_bucket_versioning" "tfstate_versioning" {
-  bucket = minio_s3_bucket.tfstate.bucket
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
+// User definitions
 resource "minio_iam_user" "buildbot" {
   name          = "buildbot"
   force_destroy = true
