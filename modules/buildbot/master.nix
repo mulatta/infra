166f16c347391333b3c20dab8077694a64149ab1
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-12 20:54:51 +0900

    drop deploy logic - let all the node use harmonia

--- modules/buildbot/master.nix
+++ modules/buildbot/master.nix
@@ -32,37 +32,6 @@ in
 
     authBackend = "github";
     admins = [ "mulatta" ];
-
-    postBuildSteps = [
-      {
-        name = "Deploy to host";
-        command = [
-          "sh"
-          "-c"
-          {
-            _type = "interpolate";
-            # Only deploy for nixos-* checks, extract host name and deploy
-            # attr format: "x86_64-linux.nixos-rho" -> host: "rho"
-            value = ''
-              ATTR="%(prop:attr)s"
-              if [[ "$ATTR" == *.nixos-* ]]; then
-                HOST="''${ATTR##*.nixos-}"
-                if [[ "$HOST" == "psi" ]]; then
-                  # psi is buildbot worker host, skip auto-deploy (manual deployment required)
-                  echo "Skipping psi deployment (manual deployment required)"
-                else
-                  echo "Deploying to $HOST..."
-                  deploy --skip-checks "github:%(prop:project)s/%(prop:revision)s#$HOST"
-                fi
-              else
-                echo "Skipping deployment for non-nixos check: $ATTR"
-              fi
-            '';
-          }
-        ];
-        warnOnly = true; # don't fail the build if deployment fails
-      }
-    ];
   };
 
   services.buildbot-master.dbUrl = lib.mkForce "postgresql://buildbot@${hosts.rho.wg-serv}/buildbot";
