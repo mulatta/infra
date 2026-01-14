37cf830f1b02cfccf64526cc43df2409d32aa25c
mulatta <67085791+mulatta@users.noreply.github.com>
2026-01-14 18:55:37 +0900

    use directory style in packages

--- packages/kexec-installer/default.nix
+++ packages/kexec-installer/default.nix
@@ -3,17 +3,22 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   commonModules = {
     imports = [
       self.inputs.nixos-images.nixosModules.kexec-installer
       self.inputs.nixos-images.nixosModules.noninteractive
       {
         system.kexec-installer.name = "nixos-kexec-installer-noninteractive";
-        services.openssh.ports = [10022];
-        boot.supportedFilesystems = lib.mkForce ["ext4" "xfs" "btrfs"];
+        services.openssh.ports = [ 10022 ];
+        boot.supportedFilesystems = lib.mkForce [
+          "ext4"
+          "xfs"
+          "btrfs"
+        ];
       }
     ];
   };
 in
-  (pkgs.nixos commonModules).config.system.build.kexecInstallerTarball
+(pkgs.nixos commonModules).config.system.build.kexecInstallerTarball
