9a690b91a0c6a11c59b18b1d03e27e16643c3187
mulatta <67085791+mulatta@users.noreply.github.com>
2026-07-23 14:29:33 +0900

    packages: consume Omnigraph from rag.nix
    Keep reusable Omnigraph packaging in rag.nix so infra only owns deployment policy. Re-export the CLI, server, and Linux bundle while sharing the infra nixpkgs and treefmt inputs.

--- flake.nix
+++ flake.nix
@@ -72,6 +72,12 @@
     };
 
     # Applications.
+    rag-nix = {
+      url = "github:mulatta/rag.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.treefmt-nix.follows = "treefmt-nix";
+    };
+
     multievolve-nix = {
       url = "github:SBEE-Lab/multievolve-nix";
       inputs.nixpkgs.follows = "nixpkgs";
--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ inputs, self, ... }:
 {
   perSystem =
     {
@@ -10,10 +10,15 @@
       packages = {
         docker-auth = pkgs.callPackage ./docker_auth { };
         infra-alert-bridge = pkgs.callPackage ./infra-alert-bridge { };
+        inherit (inputs.rag-nix.packages.${pkgs.stdenv.hostPlatform.system})
+          omnigraph-cli
+          omnigraph-server
+          ;
         slack-cli = pkgs.callPackage ./slack-cli { };
         updater = pkgs.callPackage ./updater { };
       }
       // lib.optionalAttrs pkgs.stdenv.isLinux {
+        inherit (inputs.rag-nix.packages.${pkgs.stdenv.hostPlatform.system}) omnigraph;
         installer = pkgs.callPackage ./image-installer { inherit pkgs self; };
         kexec = pkgs.callPackage ./kexec-installer { inherit pkgs self; };
         text-embeddings-inference = pkgs.callPackage ./text-embeddings-inference {
